eb42f826089e3ecb2ce724df2182022acfde37f6
edwarsthat <edwarsthat@gmail.com>
2026-02-19 16:10:32 -0500

    se moficiaron configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    // Base path for deployment to GitHub Pages
-    base: '/web-ingenieria',
-    // Public site URL for RSS, sitemaps, canonical URLs, etc.
     site: 'https://edwarsthat.github.io',
     i18n: {
         defaultLocale: 'es',
